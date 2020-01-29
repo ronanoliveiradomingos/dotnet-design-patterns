12f2b393942461f6aa2e3f9bae1f3f2f268bc5f5
Ronan Domingos <ronan.domingos.ext>
2020-01-29 14:28:26 +0000

    gitignoree

---
+++ Pattern.Factory.Method/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pattern.Factory.Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
